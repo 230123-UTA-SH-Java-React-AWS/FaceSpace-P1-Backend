55649fe4fe7ac5e3a82fbbcc7c1db246892be880
sean-bellinger <onething.sean@gmail.com>
2023-02-15 16:41:05 -0800

    First Commit

---
+++ src/main/java/com/revature/facespace/FacespaceApplication.java
@@ -0,0 +1,13 @@
+package com.revature.facespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacespaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/facespace/FacespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.facespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacespaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
