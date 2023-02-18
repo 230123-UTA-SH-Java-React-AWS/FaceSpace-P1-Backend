03b2e9253064633ecd33cb1ac0600e254e18d09e
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-02-18 15:17:41 -0800

    Created comment controller and preliminary methods

---
+++ src/main/java/com/revature/facespace/controller/CommentController.java
@@ -0,0 +1,25 @@
+package com.revature.facespace.controller;
+
+import com.revature.facespace.model.Comment;
+import com.revature.facespace.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CommentController {
+    @Autowired
+    CommentRepository commentRepository;
+
+    @GetMapping("/api/comments/{postId}")
+    public List<Comment> getAllCommentsByPost(Integer postId) {
+        return commentRepository.findAllCommentsByPostId(postId);
+    }
+
+    @PostMapping("/api/comments")
+    public Comment addComment(@RequestBody Comment comment) {
+        commentRepository.save(comment);
+        return comment;
+    }
+}
