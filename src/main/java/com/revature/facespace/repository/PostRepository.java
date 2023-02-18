911fa4a68558fb4fcb7023ddb0bf3794b6a213bc
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-02-18 14:40:04 -0800

    Added method to find all posts associated with a certain user

--- src/main/java/com/revature/facespace/repository/PostRepository.java
+++ src/main/java/com/revature/facespace/repository/PostRepository.java
@@ -3,5 +3,10 @@ package com.revature.facespace.repository;
 import com.revature.facespace.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findAllPostsByProfileId(Integer profileId);
 }
