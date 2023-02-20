3bcda4b91b49c6b6ed4cee2232740fcbdbdbf5dd
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-02-20 09:17:46 -0800

    Added To-do's for updating password and login features

--- src/main/java/com/revature/facespace/controller/UserController.java
+++ src/main/java/com/revature/facespace/controller/UserController.java
@@ -5,21 +5,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
+//@CrossOrigin("") -- this is what allows front end to use same port to get
+// data. Will use later
 public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    //Retrieving a specific user
     @GetMapping("/api/users/{id}")
     public User getUserById(@PathVariable Integer id) {
         return userRepository.getById(id);
     }
 
+    //Registration/creating a new user
     @PostMapping("/api/users")
     public User addUser(@RequestBody User user) {
-        userRepository.save(user);
-        return user;
+       return userRepository.save(user);
     }
 
+    //TODO: Password reset/change. PATCH request may also work here
+    @PutMapping("/api/users/{id}")
+    public String changePassword(@PathVariable Integer id,
+                                 @RequestBody User user) {
+        Optional<User> tempUser = userRepository.findById(id);
 
+        if (tempUser.isPresent()) {
+            userRepository.save(user);
+            return "Password successfully changed!";
+        }
+        return "An error occurred.";
+    }
+
+    //TODO: Login
+    @PostMapping("/api/user/{id}")
+    public String login() {
+        return null;
+    }
 }
