67fe1f045f39f442ca36343214ef721029da1ea2
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-02-20 07:12:57 -0800

    Created Likes service layer with POST request to add likes and return total like count of a post

---
+++ src/main/java/com/revature/facespace/controller/LikesController.java
@@ -0,0 +1,21 @@
+package com.revature.facespace.controller;
+
+import com.revature.facespace.model.Likes;
+import com.revature.facespace.repository.LikesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LikesController {
+    @Autowired
+    LikesRepository likesRepository;
+
+    //Still need a way to verify user ID and post ID
+    @PostMapping("/api/posts/likes")
+    public int addLike(@RequestBody Likes like) {
+        likesRepository.save(like);
+        return likesRepository.countLikeByPostId(like.getPostId());
+    }
+}
