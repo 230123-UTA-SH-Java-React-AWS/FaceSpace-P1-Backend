b02db4577a68051672452a30cd8a922b68f73ee9
D-Programming-Man <40450924+D-Programming-Man@users.noreply.github.com>
2023-02-21 15:47:09 -0800

    Add files via upload
    Added some login functionality to the login method, although I suspect that we will need a JSON object instead of a String object
    in the method parameter.

--- src/main/java/com/revature/facespace/controller/UserController.java
+++ src/main/java/com/revature/facespace/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.revature.facespace.controller;
 import com.revature.facespace.model.User;
 import com.revature.facespace.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -38,9 +38,45 @@ public class UserController {
         return "An error occurred.";
     }
 
-    //TODO: Login
-    @PostMapping("/api/user/{id}")
-    public String login() {
-        return null;
+    /* 
+     * Use postman to test and send in the body. To send over data to the server,
+     * format of body to make it like this:
+     * 
+     * <email>
+     * <password>
+     * 
+     * Ideally I (Andrew) will need to change that String datatype to a JSON datatype since
+     * the server expects that. I'll try to see if I can improve on this. 
+     * 
+    */
+    @GetMapping("/api/login")
+    public String login(@RequestBody String body) {
+    
+        // Login message if the user has just visited the page with no info sent to the server 
+        if (body.isBlank()) {
+            return "Welcome to the login page :3";
+        }
+
+        // Does the validation for the login credentials
+        try {
+            String[] a = body.split("\n");
+            User u = userRepository.findByEmailAddressAndPassword(a[0].strip(), a[1].strip());
+            
+            // Need this check here to see if the server sent us a null value
+            if (!u.equals(null)) {
+                return "Welcome " + u.getGivenName() + " "  + u.getSurname();
+            }
+        }
+
+        // For the String[]
+        catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+
+        // See if the server sent us a null value back
+        catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return "Login Failed";
     }
 }
--- src/main/java/com/revature/facespace/repository/UserRepository.java
+++ src/main/java/com/revature/facespace/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.revature.facespace.repository;
 
 import com.revature.facespace.model.User;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
+    User findByEmailAddressAndPassword(String emailAddress, String password);
 }
